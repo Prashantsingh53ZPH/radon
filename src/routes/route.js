d5df24c839b73ad1b38f334952f54714a0a5c169
Prashantsingh53ZPH <sin.prashant92@gmail.com>
2022-06-02 23:24:38 +0530

    Updated Value

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,7 @@
+const ChunkHandler = require('chunk-handler');
 const express = require('express');
+const  fromPairs  = require('lodash');
+const  chunk  = require('lodash');
 const externalModule = require('./logger')
 
 const router = express.Router();
@@ -10,21 +13,34 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
-router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
-});
+router.get('/hello', function (req, res) {
+    
+     const monthname= ['January','February','March','April','May','June','July','August','September','October','November','December']
+    
+    var ch = new ChunkHandler()
+    var result= ch.make(monthname,3);
+    console.log(result)
+    
+    let oddnumber= [1,3,5,7,11,13,17,19,23,29]
+    let newArray = chunk.tail(oddnumber);
+    console.log(newArray);
 
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
+    let arr1=[21,22]
+    let arr2=[22,23]
+    let arr3=[25,26]
+    let arr4=[26,27]
+    let arr5=[27,28]
 
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
+    var number= chunk.union(arr1,arr2,arr3,arr4,arr5)
+    console.log(number)
 
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
+    let movies=[["Horror","The shining"],["Drama","Titanic"],["Thriller","Shutter Island"],["Fantasy","Pans Labyrinth"]]
+    var pairs= chunk.fromPairs(movies)
+    console.log(pairs)
+
+    res.send('My first ever api!')
+   });
+
 
 module.exports = router;
 // adding this comment for no reason
